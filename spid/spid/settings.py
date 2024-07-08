08b1c2f4a3cb3c26b6014e9e0648725e6dd9ffdc
gardnera3 <71507240+gardnera3@users.noreply.github.com>
2024-07-08 09:53:59 -0400

    auto throttle and delay
    I added a 3 second delay and auto throttle feature which allows the program to dodge blocking mechanisms and scrape ethically and efficiently without force quitting. UNFORTUNATELY we now have a malformed disk error and so far the program is scraping indefinitely although I'm still testing it.
    (she autos on my throttle 'til I down load delay for 3 seconds)

--- spid/spid/settings.py
+++ spid/spid/settings.py
@@ -24,7 +24,8 @@ DEPTH_LIMIT = 1
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3 #3 second delay
+AUTOTHROTTLE_ENABLED = True #Automatic Throttle 
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
