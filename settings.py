2fc85dad3d4dcc06bb1b63104b4ec713ad9797ab
gardnera3 <71507240+gardnera3@users.noreply.github.com>
2024-07-17 16:15:01 -0400

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,131 @@
+# Scrapy settings for spid project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://docs.scrapy.org/en/latest/topics/settings.html
+#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = "spid"
+
+#output to rawdata.json
+FEEDS = {
+    'rawdata.json' : {'format' : 'json'},
+}
+# FEED_FORMAT = 'json'  # or 'csv' or 'xml' based on your preference
+# FEED_URI = 'rawdata.json'  # or 'output.csv' or 'output.xml'
+
+#SPIDER_MODULES = ["spid.spiders"]
+#NEWSPIDER_MODULE = "spid.spiders"
+DEPTH_LIMIT = 1
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = "spid (+http://www.yourdomain.com)"
+
+# Obey robots.txt rules
+##ROBOTSTXT_OBEY = True
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+#CONCURRENT_REQUESTS = 32
+
+# Configure a delay for requests for the same website (default: 0)
+# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+DOWNLOAD_DELAY = 2 #2 second delay
+AUTOTHROTTLE_ENABLED = True #Automatic Throttle 
+# The download delay setting will honor only one of:
+#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+#CONCURRENT_REQUESTS_PER_IP = 16
+
+# Disable cookies (enabled by default)
+#COOKIES_ENABLED = False
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+#DEFAULT_REQUEST_HEADERS = {
+#    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+#    "Accept-Language": "en",
+#}
+
+# Enable or disable spider middlewares
+# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+#SPIDER_MIDDLEWARES = {
+ #   "spid.middlewares.SpidSpiderMiddleware": 543,
+#}
+
+# Enable or disable downloader middlewares
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+DOWNLOADER_MIDDLEWARES = {
+    #'spid.middlewares.SpidDownloaderMiddleware': 543,
+    # 'rotating_proxies.middlewares.RotatingProxyMiddleware': 610,
+    # 'rotating_proxies.middlewares.BanDetectionMiddleware': 620,
+    "spid.spid.middlewares.MyProxyMiddleware": 350, 
+    'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 400, 
+    'scrapy.downloadermiddlewares.offsite.OffsiteMiddleware': None,
+}
+
+
+
+# Enable or disable extensions
+# See https://docs.scrapy.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    "scrapy.extensions.telnet.TelnetConsole": None,
+#}
+
+# Configure item pipelines
+# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+#ITEM_PIPELINES = {
+#    "spid.pipelines.SpidPipeline": 300,
+#}
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
+#AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+#AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+#AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+#AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = "httpcache"
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
+HTTPERROR_ALLOWED_CODES = [301,302]
+# Set settings whose default value is deprecated to a future-proof value
+REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
+TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
+FEED_EXPORT_ENCODING = "utf-8"
+
+
+PROXY_USER = "fqzoadia-rotate"
+PROXY_PASSWORD = 'tbr2g5705loa'
+PROXY_ENDPOINT = 'p.webshare.io'
+PROXY_PORT = '80'
+
+
+
+
+
+# ROTATING_PROXY_LIST = [
+#     '38.154.227.167:5868',
+#     '185.199.229.156:7492',
+#     '185.199.228.220:7300',
+#     '185.199.231.45:8382',
+#     '188.74.210.207:6286',
+#     '188.74.183.10:8279',
+#     '188.74.210.21:6100',
+#     '45.155.68.129:8133',
+#     '154.95.36.199:6893',
+#     '45.94.47.66:8110',
+# ]
+
