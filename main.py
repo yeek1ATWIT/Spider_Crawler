3404fa9362f1e2765484e62967addd9baf4cd91d
gardnera3 <71507240+gardnera3@users.noreply.github.com>
2024-07-17 16:17:28 -0400

    Main
    main

--- main.py
+++ main.py
@@ -7,10 +7,26 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.spiders import CrawlSpider, Rule
 from spid.spid.spiders.crawling_spider import CrawlingSpider
 from scrapy.linkextractors import LinkExtractor
+import importlib
+import subprocess
+
 """
 TODO
 Run this one. This File.
 """
+import search
+from scrapy.crawler import CrawlerProcess
+from scrapy.spiders import CrawlSpider, Rule
+from spid.spid.spiders.crawling_spider import CrawlingSpider
+from scrapy.linkextractors import LinkExtractor
+import spid.spid.settings as settings_module
+list1 = ["0","1","2","3","4","5","6","7","8","9","10","11","12","13"]
+list2 = ["0","1","2","3","4","5"]
+search_query2 = search.Search(list1, list2) 
+settings = {attr: getattr(settings_module, attr) for attr in dir(settings_module) if not attr.startswith("__")}
+process = CrawlerProcess(settings)
+process.crawl(CrawlingSpider, search_query=search_query2)
+process.start()
 
 def main():
 #Run UI
@@ -24,9 +40,14 @@ def main():
 #run spider--------------------------------------------------------------------------------!
 process = CrawlerProcess(settings={
         "FEEDS": {
-            "items.json": {"format": "json"},
+            "items.json": {"format": "json",},
         },
     })
+    
+def run_spider(settings,search_query2):
+    process = CrawlerProcess(settings)
+    process.crawl(CrawlingSpider, search_query=search_query2)
+    process.start()
 
 #process.crawl(CrawlSpider)
 #process.start()
@@ -39,3 +60,4 @@ process = CrawlerProcess(settings={
 if __name__ == '__main__':
     main()
     print("moo")
+        
