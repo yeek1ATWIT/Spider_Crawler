196e2e36ed8663a8535ac372346256e82ad4a0f3
yeek1ATWIT <yeek1@wit.edu>
2024-07-21 15:36:25 -0400

    Update crawling_spider.py

--- spid/spid/spiders/crawling_spider.py
+++ spid/spid/spiders/crawling_spider.py
@@ -3,19 +3,33 @@ from scrapy.linkextractors import LinkExtractor
 import queryURLsv3 as query
 import search
 import database as db
+from bs4 import BeautifulSoup
+import json
 
 class CrawlingSpider(CrawlSpider):
     name = "spidey"
     #allowed_domains = ["whitepages.com", 'privateeye.com', '.com']
-
+    
     def __init__(self, search_query=None, *args, **kwargs):
         self.search_query = search_query
         self.start_urls = []
         urls = query.get_URLs_from_file("formattedSearchURLs.txt")
-        for url in urls:
-            moo = query.fill_in_URL(url, search_query)
-            self.start_urls.append(f"{moo}")
         
+        # TODO README
+        # YOU CAN SET 'enableInput' TO TRUE OR FALSE
+        # IF TRUE, IT ADLIBS THE URLS ON "formattedSearchURLs.txt"
+        # IF FALSE, IT USES THE URLS ON "formattedSearchURLs.txt" DIRECTLY
+        # RIGHT NOT IT'S SET TO FALSE, SO IT'S NOT READING ANY CREDENTIALS SUBMITTED TO THE UI
+        enableInput = False
+        if enableInput:
+            for url in urls:
+                moo = query.fill_in_URL(url, search_query)
+                self.start_urls.append(f"{moo}")
+        else:
+            for url in urls:
+                self.start_urls.append(f"{url}")
+        #----------------------------------------------#
+
         # Setting rules dynamically based on the provided search query
         self.rules = (
             Rule(LinkExtractor(allow=()), callback='parse_item', follow=True),
@@ -23,6 +37,7 @@ class CrawlingSpider(CrawlSpider):
         
         super(CrawlingSpider, self).__init__(*args, **kwargs)
         self._compile_rules()
+        self.already_done = False
 
     def update_relevance_score(self, search_query, document, picture_in_document=False):
         document_info_lower = document.info.lower()
@@ -65,20 +80,132 @@ class CrawlingSpider(CrawlSpider):
             document.picture_relevance_score = search_query.picture.weight
         document.relevance_score += document.picture_relevance_score
 
+    def extract_text_from_html2(self, text):
+        try:
+            soup = BeautifulSoup(text, 'html.parser')
+        except:
+            return []
 
+        def extract_text(tag):
+            result = []
+            text = tag.get_text(separator='\n').strip()
+            if text:
+                result.append(' '.join(text.split()))
+            for child in tag.children:
+                if child.name:
+                    result.extend(extract_text(child))
+            return result
+
+        def flatten_list(nested_list):
+            return [item for sublist in nested_list for item in (flatten_list(sublist) if isinstance(sublist, list) else [sublist])]
+
+        text_list = []
+        html_tags = [
+            "h1", "h2", "h3", "h4", "h5", "h6",
+            "p", "span", "strong", "em", "b", "i",
+            "div", "section", "article", "header", "footer", "aside",
+            "ul", "ol", "li",
+            "table", "tr", "td", "th",
+            "a",
+            "dl", "dt", "dd",
+            "address", "figure", "figcaption"
+        ]
+        for tag in soup.find_all(html_tags):
+            text_list.extend(extract_text(tag))
+
+        flat_list = flatten_list(text_list)
+        result = []
+        for i in range(len(flat_list)):
+
+            result.append("INDEX "+str(i)+": "+flat_list[i])
+        return result
+
+    def ishardcoded(self, url):
+        hardcoded_urls = list(query.get_URLs_from_file("hardcodedURLs.txt"))
+        #if "website.com" in hardcoded_urls[0]:
+        #    hardcoded_urls.remove() # Removes sample
+        compare_urls = []
+        result = []
+
+        stur = url.replace("https://", "").replace("http://", "").replace("www.", "")   
+        for hardcoded_url in hardcoded_urls:
+            hardcoded_url_format = hardcoded_url.split(" ", 1)[0].replace("https://", "").replace("http://", "").replace("www.", "")
+            if hardcoded_url_format in stur:
+                compare_urls.append(hardcoded_url.split(' ', 1)[0])
+                result.append(hardcoded_url)
+        if compare_urls:
+            r_index = compare_urls.index(min(compare_urls, key=len))
+        
+        if result:
+            return result[r_index]
+        
+        return None
+        
     def parse_item(self, response):
         document = search.Document(response.url)
         document.info = query.highlight_names2(query.extract_text_from_html2(document.url, self.search_query))
         #query.updateRelevance(self.search_query, document)
+    
+        # HERE'S THE NAME AND PHONE IF YOU STILL WANT IT
+        name = response.css('b::text').get()
+        phone = response.css('h1::text').get()
+        if name and phone:
+            document.info = "Name: "+name+"<br>Phone: "+phone
+        if name:
+            document.info = "Name: "+name+"<br>Phone: "
+        if phone:
+            document.info = "Name: "+"<br>Phone: "+phone
+        else:
+            document.info = "Name: "+"<br>Phone: "
+        document.info += '<br>'
+        #---------------------------------------------#
+
+        document.info += '<br>'.join(self.extract_text_from_html2(response.text))
+
+        ## NOTE FOR ME THIS IS REAL CODE
         self.update_relevance_score(self.search_query, document)
         if document.info:
             db.save_document_to_db(document)
-        
-        address = response.css("div.addressline-1").get()
-        relative = response.css("div.size-aware-h5.fw-m.primary--text").get()
+        ##--------------------------------
 
-        yield {
-            "url": response.url,
-            "address": address,
-            "relative": relative,
-        }
+        # HARDCODED CODE
+        urls = query.get_URLs_from_file("formattedSearchURLs.txt")
+        stur = response.url.replace("https://", "").replace("http://", "").replace("www.", "") 
+        
+        hardcoded_urls = query.get_URLs_from_file("hardcodedURLs.txt")
+
+        jeff =  any(s.split(" ", 1)[0].replace("https://", "").replace("http://", "").replace("www.", "") in stur for s in hardcoded_urls)
+
+        stur = response.url.replace("https://", "").replace("http://", "").replace("www.", "") 
+        
+        if jeff and not self.already_done:
+            self.already_done = True
+            hardcoded_stuff = self.ishardcoded(response.url)
+            words = hardcoded_stuff.split(' ')
+            table = self.extract_text_from_html2(response.text)
+
+            name2 = json.loads(words[1])
+            phone2 = json.loads(words[2])
+            address2 = json.loads(words[3])
+
+            def find(indexes):
+                output = ""
+                for index in indexes:
+                    output += "INDEX "+str(index)+": "
+                    output += table[int(index)].split(": ", 1)[1]
+                    output += "\n"
+                return output
+            result = "ALREADY HARDCODED!\n"
+            result += "Name:\n"+find(name2)
+            result += "Phone:\n"+find(phone2)
+            result += "Address:\n"+find(address2)
+
+            g = open("hardcodedOutput.txt", "w")
+            g.write(result)
+            g.close() 
+            
+        elif any(stur in s for s in urls):
+            f = open("hardcodedOutput.txt", "w")
+            f.write(document.info.replace("<br>", "\n"))
+            f.close()
+
